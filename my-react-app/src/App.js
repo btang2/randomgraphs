c7f678b1d755aa657fccece4a25c2de5a425d8f6
brucert2 <brucert2@illinois.edu>
2025-01-06 16:44:48 -0600

    basic framework minus graph component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -4,53 +4,114 @@ import React, {useState} from 'react';
 
 
 
-function GenerateButton() {
-  return (
-      <button>generate a random graph</button>
-  );
+var button = document.getElementById("generate");
+let cooldownTime= 2000;
+let isOnCooldown = false;
+
+var n = 1;
+var p = 0.00;
+
+
+function generateGraph(n, p) {
+  //actually build the graph here
+  var adjlist = new Array(n);
+  for (var i = 0; i < n; i++) {
+    adjlist[i] = new Array(n);
+    for (var j = 0; j < n; j++) {
+      let sample = Math.random();
+      if (sample <= p) {
+        adjlist[i][j] = 1;
+      } else {
+        adjlist[i][j] = 0;
+      }
+    }
+  }
+  //i could use a preset library here but what's the fun in that
+  var canvas = document.getElementById("graphenv");
+  canvas.innerText = adjlist.map(row => row.join(', ')).join('\n');
+
 }
 
-function NRangeSlider() {
-  const [value, setValue] = useState(0); 
-  const MAX = 100; 
-  const getBackgroundSize = () => { 
-  return { backgroundSize: `${(value * 100) / MAX}% 100%` }; }; 
 
+
+button.addEventListener("click", () => {
+  if (!isOnCooldown) {
+    //now i just need to write a function for it and add environment
+    alert("generating your graph (n=" + n + ", p=" + p + ")"); 
+    generateGraph(n, p);
+    isOnCooldown = true;
+    button.disabled = true;
+    setTimeout(() => {
+      isOnCooldown = false;
+      button.disabled = false;
+    }, cooldownTime);
+  }
+});
+
+function NRangeSlider() {
+  let [value, setValue] = useState(1); 
+  const MAX = 99; 
+  const getBackgroundSize = () => { 
+  return { backgroundSize: `${((value-1) * 100) / MAX}% 100%` }; }; 
+  const getValue = () => {
+    n = value;
+    return value;
+  }
+  //weird centering thing causing elements to move around (unsolved)
   return (
+    <div>
+    <span>n: </span>
     <input type="range" 
-    min="0" 
+    id="nslider"
+    min="1" 
     max={MAX} 
     onChange={(e) => setValue(e.target.value)} 
     style={getBackgroundSize()} value={value} 
     />
+    <span>{getValue()}</span>
+    
+    </div>
   );
 }
 
 function PRangeSlider() {
-  const [value, setValue] = useState(0); 
+  let [value, setValue] = useState(0.00); 
   const MAX = 1; 
   const getBackgroundSize = () => { 
   return { backgroundSize: `${(value * 100) / MAX}% 100%` }; }; 
-
+  const getValue = () => {
+    p = value;
+    return value;
+  }
+  
   return (
+    <div>
+    <span>p: </span>
     <input type="range" 
-    min="0" 
+    id="pslider"
+    min="0.00" 
     max={MAX} 
     onChange={(e) => setValue(e.target.value)} 
-    style={getBackgroundSize()} value={value} 
-    step="0.01"
+    style={getBackgroundSize()} value={value}
+    step="0.01" 
     />
+    <span>{getValue()}</span>
+    
+    
+    </div>
   );
 }
+
 
 
 export default function MyApp() {
   return (
       <div>
         <NRangeSlider />
+        
         <PRangeSlider />
         <br></br>
-        <GenerateButton />
+        
       </div>
   );
 }
